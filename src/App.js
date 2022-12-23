155d1b2d7d4193118da7ea1cc97e91d5e1f94f34
Roshan <its.roshanojha@gmail.com>
2022-12-23 10:43:30 +0530

    Getting started with the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router =  createBrowserRouter([
+  {
+    path:"/",
+    element: <span>Home</span>
+  },
+  {
+    path:"/products/:id",
+    element: <span>Category</span>
+  },
+   {
+    path:"/product/:id",
+    element: <span>Product</span>
+  }
+])
+
 function App() {
   return (
     <div>
-      Hello world!
+       <RouterProvider router={router} />
     </div>
   );
 }

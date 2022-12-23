c7577e83562085cf065a64d39fa3ab5ffedac1e2
Roshan <its.roshanojha@gmail.com>
2022-12-23 11:19:01 +0530

    Created Navbar and Footer components and used the Outlet feature of React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import {createBrowserRouter, RouterProvider,Route,Link, } from "react-router-dom";
+import {createBrowserRouter, RouterProvider,Route,Link, Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Product from "./pages/Product/Product";
 import Products from "./pages/Products/Products";
 
+// creating Layouts
+const Layouts = () => {
+  return(
+    <div className="app">
+      <Navbar/>
+      <Outlet /> {/* It represents our different pages  */}
+      <Footer />
+    </div>
+  )
+}
+
+
+// creating router here
 const router =  createBrowserRouter([
   {
     path:"/",
-    element: <Home />
-  },
-  {
-    path:"/products/:id",
-    element: <Products />
-  },
-   {
-    path:"/product/:id",
-    element: <span>Product</span>
+    element: <Layouts />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>
+      }
+    ]
   }
 ])
 

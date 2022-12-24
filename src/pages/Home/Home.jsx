a3f6f13277cdd16885fd2a427abdd7a9243f235e
Roshan <its.roshanojha@gmail.com>
2022-12-24 15:46:56 +0530

    Created Contact component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Categories from '../../components/Categories/Categories'
+import Contact from '../../components/Contact/Contact'
 import FeaturedProduct from '../../components/FeaturedProduct/FeaturedProduct'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
@@ -11,6 +12,7 @@ const home = () => {
         <FeaturedProduct type="featured"/>
         <Categories/>
         <FeaturedProduct type="trending"/>
+        <Contact/>
 
     </div>
   )

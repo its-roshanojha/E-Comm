2df00b7d48590a9e63cb2fea214d91dd6daa3b03
Roshan <its.roshanojha@gmail.com>
2022-12-24 23:35:49 +0530

    Created List component and Product page

---
+++ src/components/List/List.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Card from '../Card/Card'
+import "./List.scss"
+
+const List = () => {
+    const data =  [
+        {
+            id: 1,
+            img: "https://images.bewakoof.com/t1080/black-hoodie-sweatshirt-363701-1669641515-1.jpg",
+            img2: "https://images.bewakoof.com/t1080/black-hoodie-sweatshirt-363701-1669641526-3.jpg",
+            title: "Men's Black Hoodie",
+            isNew: true,
+            oldPrice: 2399,
+            price: 1199
+        },
+        {
+          id: 2,
+          img: "https://images.bewakoof.com/t1080/women-party-fav-tee-oversized-printed-t-shirt-554322-1671088258-1.jpg",
+          img2: "https://images.bewakoof.com/t1080/women-party-fav-tee-oversized-printed-t-shirt-554322-1671088263-2.jpg",
+          title: "Women's Bold Red Party Fav Typography Oversized T-shirt",
+          isNew: true,
+          oldPrice: 2599,
+          price: 1699
+      },
+      {
+        id: 3,
+        img: "https://images.bewakoof.com/t1080/men-s-black-uchiha-s-unsung-hero-graphic-printed-t-shirt-495314-1656160537-1.jpg",
+        img2: "https://images.bewakoof.com/t1080/men-s-black-uchiha-s-unsung-hero-graphic-printed-t-shirt-495314-1656160543-2.jpg",
+        title: "Men's Black Uchiha's Unsung Hero Graphic Printed T-shirt",
+        isNew: false,
+        oldPrice: 1599,
+        price: 999
+    },
+    {
+      id: 4,
+      img: "https://images.bewakoof.com/t1080/men-s-black-gojo-domain-graphic-printed-oversized-t-shirt-520746-1667495708-2.jpg",
+      img2: "https://images.bewakoof.com/t1080/men-s-black-gojo-domain-graphic-printed-oversized-t-shirt-520746-1667495714-3.jpg",
+      title: "Men's Black Gojo Domain Graphic Printed Oversized T-shirt",
+      isNew: false,
+      oldPrice: 1699,
+      price: 1299
+    },
+    ]
+  return (
+    <div className='list'>{data?.map(item =>(
+        <Card item = {item}  key={item.id}/>
+    ))}
+      
+    </div>
+  )
+}
+
+export default List
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,12 +1,103 @@
-import React from 'react'
-import "./Products.scss"
+import React from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import List from "../../components/List/List";
+import "./Products.scss";
 
-const products = () => {
+const Products = () => {
+
+  const catId = parseInt(useParams().id);
+  // console.log(catId);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null);
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
+
   return (
-    <div className='products'>
-      
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          
+            <div className="inputItem">
+              <input
+                type="checkbox"
+                id="1"
+                value={1}
+              />
+              <label htmlFor={1}>Womens</label>
+            </div>
+            <div className="inputItem">
+              <input
+                type="checkbox"
+                id="2"
+                value={2}
+              />
+              <label htmlFor={2}>Men</label>
+            </div>
+            <div className="inputItem">
+              <input
+                type="checkbox"
+                id="3"
+                value={3}
+              />
+              <label htmlFor={3}>shoes</label>
+            </div>
+          
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input
+              type="range"
+              min={0}
+              max={10000}
+              onChange={(e) => setMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
+            <label htmlFor="asc">Price (Lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
+            <label htmlFor="desc">Price (Highest first)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.bewakoof.com/uploads/grid/app/Desktop-Strip-Common-1669986431.jpg"
+          alt=""
+        />
+        <img
+          className="ambassador"
+          src="https://cdn.shopify.com/s/files/1/0420/7073/7058/files/Purple_Green_Founder_Quote_Email_Header_2000_x_1000px_2000_x_80px_1600x.jpg?v=1664543529"
+          alt=""
+        />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats}/>
+
+        
+      </div>
     </div>
   )
 }
 
-export default products
+export default Products
